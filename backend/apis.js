b3f46defd1c7fd077c8e8ea6c09dfa1960ecaaaa
Gabriel Zhang <gabriel.b.zhang@gmail.com>
2024-04-21 14:42:47 -0700

    Feat: delete record

--- backend/apis.js
+++ backend/apis.js
@@ -14,6 +14,7 @@ export const redirect = async (req, res) => {
 export const create = async (req, res) => {
   const recordExists = await UrlPair.exists({ short: req.body.short });
   if (recordExists) {
+    console.log(`Short URL: [${req.body.short}] already exists in records.`);
     res.sendStatus(409);
     return;
   }
@@ -21,7 +22,26 @@ export const create = async (req, res) => {
     .then(() => {
       res.sendStatus(201);
     })
-    .catch(() => {
+    .catch((err) => {
+      console.error(err);
       res.sendStatus(409);
+    });
+};
+
+export const remove = async (req, res) => {
+  const query = { short: req.params.shortUrl };
+  const recordExists = await UrlPair.exists(query);
+  if (!recordExists) {
+    res.sendStatus(404);
+    return;
+  }
+  const record = await UrlPair.findOne(query);
+  UrlPair.deleteOne(record)
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(404);
     });
 };
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import { connect } from "mongoose";
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import cors from "cors"
 
 dotenv.config();
 const mongodbUser = process.env.MONGODB_USER;
@@ -9,6 +10,7 @@ const mongodbPassword = process.env.MONGODB_PASSWORD;
 const port = process.env.PORT;
 
 const app = express();
+app.use(cors())
 const uri = `mongodb+srv://${mongodbUser}:${mongodbPassword}@cluster0.huleewa.mongodb.net/data?retryWrites=true&w=majority&appName=Cluster0`;
 connect(uri)
   .then(() => {
@@ -21,7 +23,8 @@ connect(uri)
     console.error(err);
   });
 
-import { redirect, create } from "./apis.js";
+import { redirect, create, remove } from "./apis.js";
 
 app.get("/:shortUrl", redirect);
 app.post("/api/create", bodyParser.json(), create);
+app.delete("/api/delete/:shortUrl", remove);
